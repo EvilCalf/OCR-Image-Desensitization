c70c09604f129705b7d53167cd322eb7b8dd7840
EvilCalf <xujiading1997@163.com>
2019-12-11 16:53:29 +0800

    enhanced robustness

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ tesseract_cmd = r".\tesseract-ocr\tesseract.exe"
 
 for root, dirs, files in os.walk("labimage/"):
     for file in files:
+        imgaetype = 1
         image = Image.open(root + "/" + file)
-        content = pytesseract.image_to_data(image, lang="chi_sim43", output_type="dict")
+        if image.size[0] == 4032:
+            image = image.rotate(-90)
+            imgaetype = 2
+        content = pytesseract.image_to_data(image, lang="chi_sim12", output_type="dict")
         for i in range(len(content["text"])):
             if 0 < len(content["text"][i]):
-                if content["text"][i] == "姓名" or (
-                    content["text"][i] == "姓" and content["text"][i + 1] == "名"
-                ):
+                if content["text"][i] == "姓名" or content["text"][i] == "姓":
                     (x, y, w, h) = (
                         content["left"][i],
                         content["top"][i],
@@ -24,7 +26,11 @@ for root, dirs, files in os.walk("labimage/"):
                         content["height"][i],
                     )
                     print(x, y, w, h)
-                    img = image.crop((x - 10, y - 10, x + w + 400, y + h + 30))
+                    if imgaetype == 1:
+                        box = (x - 10, y - 10, x + w + 400, y + h + 30)
+                    else:
+                        box = (x - 5, y - 5, x + w + 400, y + h + 10)
+                    img = image.crop(box)
                     content = pytesseract.image_to_string(
                         img, lang="chi_sim43", output_type="dict"
                     )
@@ -35,11 +41,18 @@ for root, dirs, files in os.walk("labimage/"):
                         content = pytesseract.image_to_string(
                             img, lang="chi_sim43", output_type="dict"
                         )
+                    elif content["text"][0] != "姓":
+                        filename = "./Cache/" + file
+                        img.save(filename)
+                        img = cv2.imread(filename)
+                        content = pytesseract.image_to_string(
+                            img, lang="chi_sim12", output_type="dict"
+                        )
                     cnt = content["text"]
                     cnt = cnt.replace(" ", "")
                     cnt = cnt[3:]
                     print(cnt)
-                    image.paste((0, 0, 0), (x - 10, y - 10, x + w + 400, y + h + 30))
+                    image.paste((0, 0, 0), box)
                     image.save("./Output/" + file)
                     data = pd.DataFrame({"name": [cnt], "dir": [root + "/" + file]})
                     data.to_csv("name2file.csv", mode="a", header=False)
